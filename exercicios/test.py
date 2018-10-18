eca7c569ad5f055e989222f17253ef40ec81902f
Clemencia Mora Herrera <clemencia.mora.herrera@cern.ch>
2018-10-18 13:57:01 -0300

    Create test.py

---
+++ exercicios/test.py
@@ -0,0 +1,13 @@
+#primeiro exemplo de funções
+def meuverso(palavras):
+    print(palavras)
+    print(palavras)
+    print(gato)
+
+def gato_duplo (parte1, parte2): 
+    gato = parte1 + parte2 
+    meuverso (gato)
+
+linha1= "miau"
+linha2= "grrr miau"
+gato_duplo(linha1,linha2)
